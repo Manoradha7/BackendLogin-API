da3fc65358c11af89bcd0c21a12d8985e7e3ae46
Manoradha7 <manoradha7@gmail.com>
2022-01-06 08:32:40 +0530

    comments add

--- helper.js
+++ helper.js
@@ -1,6 +1,6 @@
 import bcrypt from "bcrypt";
 import { client } from "./index.js";
-
+//generate hashpassword 
  async function genPassword(password){
     const no_of_rounds =10;
     const salt = await bcrypt.genSalt(no_of_rounds);
@@ -15,26 +15,31 @@ async function passwordUpdate(data)
     let result=await client.db('primestar').collection('users').updateOne({email},{$set:{password:token}})
     return result
 }
-
+//inserting userb details in Database
 async function createUser(name, email, hashedPassword) {
   return client.db("primestar").collection("users").insertOne({ name, email, password: hashedPassword });
 }
-
+// finduser user data using email 
 async function getUser(email) {
   return client.db("primestar").collection("users").findOne({ email });
 }
-
-async function updateuser(data)
+//find user data 
+async function getuser(values) {
+  return client.db("primestar").collection("users").findOne(values);
+}
+// updating user data password
+async function updateuser(values)
 {
-    const{email,Password}= data
+    const{email,Password}=values
     let result=await client.db('primestar').collection('users').updateOne({email},{$set:{Password:Password}})
     return result;
 }
-
+//exporting the components
 export{
     genPassword,
     passwordUpdate,
     createUser,
     getUser,
+    getuser,
     updateuser
 }
--- index.js
+++ index.js
@@ -8,17 +8,20 @@ import {
   passwordUpdate,
   createUser,
   getUser,
+  getuser,
   updateuser,
 } from "./helper.js";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
 import nodemailer from "nodemailer";
+import cors from 'cors';
   
 // creating new express app and save it as app
 const app = express();
 
 // Middleware
 dotenv.config();
+app.use(cors())
 app.use(express.json());
 app.use(
   bodyParser.urlencoded({
@@ -29,9 +32,9 @@ app.use(bodyParser.json());
 
 // server configuration
 const PORT = process.env.PORT;
-
+//MongoDb database Url
 const MONGO_URL = process.env.MONGO_URL;
-
+// connection to the MongoDB 
 async function createConnection() {
   const client = new MongoClient(MONGO_URL);
   await client.connect();
@@ -48,37 +51,53 @@ app.get("/", async (req, res) => {
   res.send("Passwod reset flow");
 });
 
+//signup for 
 app.post("/signup", async (req, res) => {
+  // from body the user information are ten
   const { name, email, password } = req.body;
-
+ 
+  // after getting the user data checking the user are existing or not 
   const data = await getUser(email);
-
+ 
+  // if user data is in DB  retturn user existing message 
   if (data) {
     return res.status(400).send({ Message: "Email id is aldready exist" });
   }
-
+  
+  // if password length lessthan eight return long password required message
   if (password.length < 8) {
     return res.status(400).send({ Message: "Password must be longer" });
   }
-  const hashedPassword = await genPassword(password);
 
+  // generate hashpassword for the password
+  const hashedPassword = await genPassword(password);
+ 
+  //adding user data into the DATABASE
   const createUserData = await createUser(name, email, hashedPassword);
   console.log(createUserData);
   const userData = await getUser(email);
   res.send(userData);
 });
 
+// signin
 app.post("/signin", async (req, res) => {
+  //from body the user infromation are taken
   const { email, password } = req.body;
 
+  //check the mail is existing in the DB
   const data = await getUser(email);
 
+  //if the data is not avilable in the  DB then return an error message
   if (!data) {
     return res.status(400).send({ Message: "Invalid credentials" });
   }
+  // for checking the stored password and user given password 
   const dbPassword = data.password;
+ 
+  //check both password are match are not
   const isPwdMatch = await bcrypt.compare(password, dbPassword);
-
+  
+  //if password are match return success message along with token otherwise return an error message
   if (isPwdMatch) {
     const token = jwt.sign({ id: data._id }, process.env.SECRET_KEY);
     res.status(200).send({ Message: "Signin Succesfully" });
@@ -89,11 +108,14 @@ app.post("/signin", async (req, res) => {
   console.log(data);
 });
 
+// forget password
 app.post("/forgetpassword", async (req, res) => {
+ //getting the data from the body
   const { email } = req.body;
-
+  // getting the data from the Db 
   const data = await getUser(email);
   console.log(data);
+  // if there is no data throe an error message
   if (!data) {
     res.status(400).send({ Message: "Invalid credentials" });
   }
@@ -101,9 +123,10 @@ app.post("/forgetpassword", async (req, res) => {
   const token = jwt.sign({ id: data._id }, process.env.SECRET_KEY);
   //  The generated token will replace the old password
   const replacePassword = await passwordUpdate({ email, token });
-  // console.log(replacePassword);
-  let updatedResult = await getUser({email});
-  // console.log(updatedResult);
+  console.log(replacePassword);
+  let updatedResult = await getUser(email);
+  console.log(data.email,data.password);
+  console.log(updatedResult);
 
   // mail for reset the password
   Mail(token, email);
@@ -111,11 +134,17 @@ app.post("/forgetpassword", async (req, res) => {
   return res.send({ updatedResult, token });
 });
 
+//forget password
 app.get("/forgetpassword/verify", async (req, res) => {
-  const token = await req.header("x-auth-token");
+  //get the token from the header
+  const token = req.header(`x-auth-token`);
+  // console.log(token);
 
-  const tokenVerify = await getUser({ password: token });
-
+  // get the from the DB for verification both are match or not
+  const tokenVerify = await getuser({password:token});
+  // console.log({"vtoken :":tokenVerify})
+  
+  // if token is not match return an error msg otherwise matched return matched msg
   if (!tokenVerify) {
     return res.status(400).send({ Message: "Invalid Credentials" });
   } else {
@@ -123,29 +152,32 @@ app.get("/forgetpassword/verify", async (req, res) => {
   }
 });
 
+// resetpassword
 app.post("/resetpassword", async (req, res) => {
+  // get the data from the body
   const { password, token } = req.body;
-
+// If password length is less than 8 return longer password require message
   if (password.length < 8) {
     return res.status(400).send({ Message: "password must be longer" });
   }
-
+// get the data
   const data = await getUser({ password: token });
-
+// the data is not there in the DB return an error msg
   if (!data) {
     return res.status(401).send({ Message: "Invalid credentials" });
   }
   const { email } = data;
-
+// generate hashpassword  for the password
   const hashedPassword = await genPassword(password);
-
+//update the new password for the user
   const passwordUpdate = await updateuser({ email, password: hashedPassword });
-
+// get the user data 
   const result = await getUser({ email });
 
   return res.send(result);
 });
 
+// For sending mail for verification
 function Mail(token, email) {
   console.log(process.env.email,process.env.password)
   const transport = nodemailer.createTransport({
@@ -156,7 +188,7 @@ function Mail(token, email) {
     },
   });
 
-  const link = `http://localhost:8000/forgetpassword/verify/${token}`;
+  const link = `http://localhost:3000/forgetpassword/verify/${token}`;
   const mailOptions = {
     from: process.env.email,
     to: email,
