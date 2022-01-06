5e77439aee8eb6c631d2dcce77cc83eb0fff8ed6
Manoradha7 <manoradha7@gmail.com>
2022-01-06 18:12:39 +0530

    commit

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const app = express();
 // Middleware
 dotenv.config();
 const corsOptions ={
-  origin:'*', 
+  origin:'http://localhost:3000', 
   credentials:true,            //access-control-allow-credentials:true
   optionSuccessStatus:200,
 }
