b2b672e7707e9a4ce353ffc707db56f223555f19
Manoradha7 <manoradha7@gmail.com>
2022-01-06 12:43:32 +0530

    cors added

--- index.js
+++ index.js
@@ -21,7 +21,13 @@ const app = express();
 
 // Middleware
 dotenv.config();
-app.use(cors());
+const corsOptions ={
+  origin:'*', 
+  credentials:true,            //access-control-allow-credentials:true
+  optionSuccessStatus:200,
+}
+
+app.use(cors(corsOptions))
 app.use(express.json());
 app.use(
   bodyParser.urlencoded({
