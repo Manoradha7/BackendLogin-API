158fa62c244116188a37c0fb49d561270e7d4873
Manoradha7 <manoradha7@gmail.com>
2022-01-09 12:09:18 +0530

    try catch added

--- helper.js
+++ helper.js
@@ -1,46 +1,58 @@
 import bcrypt from "bcrypt";
+import res from "express/lib/response";
 import { client } from "./index.js";
-//generate hashpassword 
- async function genPassword(password){
-    const no_of_rounds =10;
-    const salt = await bcrypt.genSalt(no_of_rounds);
-    const hashedPassword = await bcrypt.hash(password,salt);
-    return hashedPassword;
-  }
+//generate hashpassword
+async function genPassword(password) {
+  const no_of_rounds = 10;
+  const salt = await bcrypt.genSalt(no_of_rounds);
+  const hashedPassword = await bcrypt.hash(password, salt);
+  return hashedPassword;
+}
 
-  // After forgot password,here the token will update the existing password
-async function passwordUpdate(data)
-{
-    let {email,token}=data
-    let result=await client.db('primestar').collection('users').updateOne({email},{$set:{password:token}})
-    return result
+// After forgot password,here the token will update the existing password
+async function passwordUpdate(data) {
+  let { email, token } = data;
+  let result = await client
+    .db("primestar")
+    .collection("users")
+    .updateOne({ email }, { $set: { password: token } });
+  return result;
 }
 //inserting userb details in Database
 async function createUser(name, email, hashedPassword) {
-  return client.db("primestar").collection("users").insertOne({ name, email, password: hashedPassword });
+  return client
+    .db("primestar")
+    .collection("users")
+    .insertOne({ name, email, password: hashedPassword });
 }
-// finduser user data using email 
+// finduser user data using email
 async function getUser(email) {
-  return client.db("primestar").collection("users").findOne({ email:email });
-  console.log(email);
+  try {
+    console.log(email);
+    return client.db("primestar").collection("users").findOne({ email: email });
+  } catch (err) {
+    res.send(err);
+  }
 }
-//find user data 
+//find user data
 async function getuser(values) {
   return client.db("primestar").collection("users").findOne(values);
 }
 // updating user data password
-async function updateuser(values)
-{
-    const{email,Password}=values
-    let result=await client.db('primestar').collection('users').updateOne({email},{$set:{Password:Password}})
-    return result;
+async function updateuser(values) {
+  const { email, Password } = values;
+  let result = await client
+    .db("primestar")
+    .collection("users")
+    .updateOne({ email }, { $set: { Password: Password } });
+  return result;
 }
 //exporting the components
-export{
-    genPassword,
-    passwordUpdate,
-    createUser,
-    getUser,
-    getuser,
-    updateuser
-}
+export {
+  genPassword,
+  passwordUpdate,
+  createUser,
+  getUser,
+  getuser,
+  updateuser,
+};
